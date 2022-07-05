a3f1f5409156a8ee7f808c5398950fbc2b4d33de
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-05 23:56:54 +0600

    working on the tabBar and tabBarView

--- lib/screens/credit_card_page_with_creditcard.dart
+++ lib/screens/credit_card_page_with_creditcard.dart
@@ -1,3 +1,5 @@
+import 'package:finance_mobile_app/widgets/constants.dart';
+import 'package:finance_mobile_app/widgets/top_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/credit_card_page.dart';
@@ -7,8 +9,33 @@ class CreditCardPageWithCreditCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CreditCardsPage(),
-    );
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          color: login_color,
+          width: width,
+          height: height / 4,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 48.0),
+          child: Container(
+            width: width,
+            height: height,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: TopTabBar(),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
---
+++ lib/widgets/top_tab_bar.dart
@@ -0,0 +1,61 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
+import 'package:flutter/material.dart';
+
+class TopTabBar extends StatefulWidget {
+  const TopTabBar({Key? key}) : super(key: key);
+
+  @override
+  _TopTabBarState createState() => _TopTabBarState();
+}
+
+class _TopTabBarState extends State<TopTabBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: Column(
+            children: <Widget>[
+              ButtonsTabBar(
+                unselectedBackgroundColor: Colors.white,
+                unselectedLabelStyle: TextStyle(color: Colors.black),
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                tabs: [
+                  Tab(
+                    text: "My Cards",
+                  ),
+                  Tab(
+                    text: "Deposits",
+                  ),
+                  Tab(
+                    text: "Transactions",
+                  ),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: <Widget>[
+                    Center(
+                      child: Icon(Icons.directions_car),
+                    ),
+                    Center(
+                      child: Icon(Icons.directions_transit),
+                    ),
+                    Center(
+                      child: Icon(Icons.directions_bike),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
