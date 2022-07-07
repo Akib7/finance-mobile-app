8093038b9618f198a2230edf2f5e510decc11689
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-07 23:38:58 +0600

    working with the topUp page

--- lib/screens/credit_card_page_with_creditcard.dart
+++ lib/screens/credit_card_page_with_creditcard.dart
@@ -6,35 +6,68 @@ import 'package:flutter/material.dart';
 class CreditCardPageWithCreditCard extends StatelessWidget {
   const CreditCardPageWithCreditCard({Key? key}) : super(key: key);
 
+  Container _buildAddCardButton({
+    required Icon icon,
+    required Color color,
+  }) {
+    return Container(
+      margin: const EdgeInsets.only(top: 24.0),
+      alignment: Alignment.center,
+      child: FloatingActionButton(
+        elevation: 2.0,
+        onPressed: () {
+          print("Add a credit card");
+        },
+        backgroundColor: color,
+        mini: false,
+        child: icon,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          color: login_color,
-          width: width,
-          height: height / 4,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 48.0),
-          child: Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(50), topRight: Radius.circular(50)),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.all(18.0),
-              child: TopTabBar(),
-            ),
+        body: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              Container(
+                color: login_color,
+                width: width,
+                height: height / 4,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 48.0),
+                child: Container(
+                  width: width,
+                  height: height,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50)),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: TopTabBar(),
+                  ),
+                ),
+              ),
+              _buildAddCardButton(
+                icon: const Icon(
+                  Icons.add,
+                ),
+                color: const Color(0xFF081603),
+              )
+            ],
           ),
-        ),
-      ],
+        ],
+      ),
     ));
   }
 }
